fc58e94ed5028a2f86e8e58195d3f1457adcdc5b
patrick chang <patrick.chang1223@gmail.com>
2023-07-20 13:19:10 +0900

    modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # terraform-homework
+
+### this repo contains 5 modules: ec2, vpc, security group, s3 and dynamodb
+</br>
+code will create the followings; </br>
+</br>
+1 vpc </br>
+4 subnets (per each az)</br>
+1 route table </br>
+1 security group </br>
+4 ec2 instances (per each az) </br>
+
+
+
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_key_pair" "key_pair" {
-    for_each = var.key_config
-    key_name = each.key
-    public_key= file(each.value.public_key_path)
-    tags = {
-        Name = each.key
-    }
+  for_each   = var.key_config
+  key_name   = each.key
+  public_key = file(each.value.public_key_path)
+  tags = {
+    Name = each.key
+  }
 }
 
 data "aws_availability_zones" "azs" {}
@@ -27,32 +27,31 @@ data "aws_availability_zones" "azs" {}
 #         name = "architecture"
 #         values = ["x86_64"]
 #     }
-  
+
 # }
 
 resource "aws_instance" "instance" {
-    count = length(data.aws_availability_zones.azs.names)
-    ami = var.ec2_config[count.index].ami
-    availability_zone = data.aws_availability_zones.azs.names[count.index]
-    ebs_optimized = true
-    instance_type = var.ec2_config[count.index].instance_type
-    key_name = var.ec2_config[count.index].key_nm
-    monitoring = var.ec2_config[count.index].monitoring
-    vpc_security_group_ids = var.security_group_ids
-    subnet_id = var.public_subnet_ids[count.index]
-    root_block_device {
-        delete_on_termination = true
-        encrypted = true
-        iops = try(var.ec2_config[count.index].root_block_iops, null)
-        volume_type = try(var.ec2_config[count.index].root_block_vol_type, null)
-        volume_size = try(var.ec2_config[count.index].root_block_vol_size, null)
-        throughput = try(var.ec2_config[count.index].root_block_throughput, null)
-        tags = {
-            Name = try(var.ec2_config[count.index].root_block_nm, null)
-        }
-    }
+  count                  = length(data.aws_availability_zones.azs.names)
+  ami                    = var.ec2_config[count.index].ami
+  availability_zone      = data.aws_availability_zones.azs.names[count.index]
+  ebs_optimized          = true
+  instance_type          = var.ec2_config[count.index].instance_type
+  key_name               = var.ec2_config[count.index].key_nm
+  monitoring             = var.ec2_config[count.index].monitoring
+  vpc_security_group_ids = var.security_group_ids
+  subnet_id              = var.public_subnet_ids[count.index]
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+    iops                  = try(var.ec2_config[count.index].root_block_iops, null)
+    volume_type           = try(var.ec2_config[count.index].root_block_vol_type, null)
+    volume_size           = try(var.ec2_config[count.index].root_block_vol_size, null)
+    throughput            = try(var.ec2_config[count.index].root_block_throughput, null)
     tags = {
-        Name = var.ec2_config[count.index].instance_nm
+      Name = try(var.ec2_config[count.index].root_block_nm, null)
     }
+  }
+  tags = {
+    Name = var.ec2_config[count.index].instance_nm
+  }
 }
-
